9fa9f1ab1d8dd769b38e0edf5ff0f666ddd736dc
AnimeKay <matthewchen17@gmail.com>
2024-05-21 23:57:30 -0400

    Add files via upload
    Needs to be tested.

---
+++ LAMPAPI/SignUp.php
@@ -0,0 +1,66 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$login = $inData["login"];
+	$password = $inData["password"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		// Find if already exists a username w/ same name
+		$sql = "SELECT * FROM Users WHERE Login=?";
+		$stmt-> $conn->prepare($sql);
+		$stmt->execute();
+		$stmt->bind_param("s", $login);
+		$stmt->execute();
+		$rows = mysqli_num_rows($result);
+		$result = $stmt->get_result();
+
+		// If not, insert into Users table
+		if($result->num_rows == 0) {
+			$stmt = $conn->prepare("INSERT INTO Users (firstName, lastName, login, password) VALUES (?, ?, ?, ?)");
+			$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+			$stmt->execute();
+			returnWithInfo("User successfully created");
+		}
+
+		// Else, just don't
+		else {
+			returnWithError("Username taken");
+		}
+		
+		$stmt->close();
+		$conn->close();
+
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
