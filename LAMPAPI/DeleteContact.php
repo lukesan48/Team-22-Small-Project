dda30389a3316bfcae4609f774374de75a78bc1a
cgomez80 <clarence.v.gomez@outlook.com>
2024-06-04 19:12:34 -0400

    Update DeleteContact.php

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,44 +1,41 @@
 <?php
-    $inData = getRequestInfo();
+$inData = getRequestInfo();
 
-    $userId = $inData["userId"];
-    $firstName = $inData["firstName"];
-    $lastName = $inData["lastName"];
-    // Database connection
-    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+$userId = $inData["userId"];
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+// Database connection
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 
-    if ($conn->connect_error)
-    {
-        returnWithError($conn->connect_error);
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    // Prepare statement
+    $stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName = ? OR LastName = ? AND UserID = ?");
+    if (!$stmt) {
+        returnWithError($conn->error);
+    } else {
+        $stmt->bind_param("ssi", $firstName, $lastName, $userId);
+        $stmt->execute();
+        $stmt->close();
     }
-    else
-    {
-        // Prepare statement
-        $stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName = ? AND LastName = ? AND UserID = ?");
-        if (!$stmt) {
-            returnWithError($conn->error);
-        } else {
-            $stmt->bind_param("ssi", $firstName, $lastName, $userId);
-            $stmt->execute();
-            $stmt->close();
-        }
-        $conn->close();
-    }
+    $conn->close();
+}
 
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
-    }
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-    function sendResultInfoAsJson($obj)
-    {
-        header('Content-type: application/json');
-        echo $obj;
-    }
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
 
-    function returnWithError($err)
-    {
-        $retValue = '{"error":"' . $err . '"}';
-        sendResultInfoAsJson($retValue);
-    }
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
 ?>
